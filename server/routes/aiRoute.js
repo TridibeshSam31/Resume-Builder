bb4a560df40c80f9586cf954f71ccbfe4739422f
TridibeshSam31 <tridisam651@gmail.com>
2025-10-17 13:32:50 +0530

    route updated

--- server/routes/aiRoute.js
+++ server/routes/aiRoute.js
@@ -1,13 +1,16 @@
 import express from "express"
 import userAuth from "../middleware/auth.middleware.js"
-import { enhanceProffesionalSummary,enhanceJobDescription,uploadResume } from "../controllers/ai.controller.js"
+import { enhanceProffesionalSummary,enhanceJobDescription,updateResume,getATSscore,analyzeAtsScore } from "../controllers/ai.controller.js"
 import { updateResume } from "../controllers/resume.controller"
 
 const aiRouter = express.Router()
 
 aiRouter.post('/enhance-pro-sum',userAuth,enhanceProffesionalSummary)
 aiRouter.post('/enhance-job-desc',userAuth,enhanceJobDescription)
-aiRouter.post('/upload-resume',ProcessingInstruction,updateResume)
+aiRouter.post('/upload-resume',userAuth,updateResume)
+aiRouter.post('/analyze',userAuth,analyzeAtsScore)
+aiRouter.get('/ats',userAuth,getATSscore)
+
 
 export default aiRouter
 
